08a7ade5263bee3335db1dbc173f6cc25bd954ea
Roman Nozdrachev <roman.nozdracheff@yandex.ru>
2021-11-13 19:55:18 +0300

    init

---
+++ src/test/java/ru/stqa/tests/selenium/MyFirstTest.java
@@ -0,0 +1,31 @@
+package ru.stqa.tests.selenium;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MyFirstTest {
+
+    private WebDriver driver = new ChromeDriver(new ChromeDriverService.Builder().withVerbose(true).build());
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver(new ChromeDriverService.Builder().withVerbose(true).build());
+        wait = new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("https://www.google.ru/");
+    }
+
+    @Test
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
