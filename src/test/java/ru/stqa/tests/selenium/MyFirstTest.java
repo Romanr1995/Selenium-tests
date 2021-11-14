187bebf8baa875619553de9ca618aa334f133ef9
Roman Nozdrachev <roman.nozdracheff@yandex.ru>
2021-11-14 23:09:35 +0300

    добавил сценарий логина

--- src/test/java/ru/stqa/tests/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/tests/selenium/MyFirstTest.java
@@ -3,6 +3,7 @@ package ru.stqa.tests.selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
@@ -16,12 +17,19 @@ public class MyFirstTest {
     @Before
     public void start() {
         driver = new ChromeDriver(new ChromeDriverService.Builder().withVerbose(true).build());
-        wait = new WebDriverWait(driver,10);
+        wait = new WebDriverWait(driver, 10);
     }
 
     @Test
     public void myFirstTest() {
-        driver.get("https://yandex.ru/");
+        try {
+            driver.get("http://localhost/litecart/admin/");
+            driver.findElement(By.name("username")).sendKeys("admin");
+            driver.findElement(By.name("password")).sendKeys("admin");
+            driver.findElement(By.name("login")).click();
+        } catch (Exception e) {
+            stop();
+        }
     }
 
     @After
